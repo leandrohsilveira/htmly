101b809338e2c47c30c8d951a5f104a43bc26022
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-02-23 22:15:03 -0300

    update vitest coverage exclusions

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite"
+import { configDefaults } from "vitest/config"
 
 export default defineConfig({
   test: {
     coverage: {
       enabled: true,
       provider: "istanbul",
-      reporter: ["text", "json", "html"]
+      reporter: ["text", "json", "html"],
+      exclude: [
+        ...(configDefaults.coverage.exclude ?? []),
+        "**/testing.js",
+        "**/testing.ts",
+        "**/testing/**",
+        "**/dist/**"
+      ]
     }
   }
 })
